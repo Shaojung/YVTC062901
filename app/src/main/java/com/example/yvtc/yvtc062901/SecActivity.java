3552e598992385c0788b9618af9e2e162f078536
Shaojung <shaojung@gmail.com>
2017-06-29 09:29:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvtc062901/SecActivity.java
@@ -0,0 +1,20 @@
+package com.example.yvtc.yvtc062901;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec);
+
+        Intent it = getIntent();
+        String str = it.getStringExtra(Intent.EXTRA_TEXT);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(str);
+    }
+}
